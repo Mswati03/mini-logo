49c6fa30120d0f9122bec8f8ce94d785156b6dda
Mswati Tshabalala <mswatitshabalala03@gmail.com>
2025-02-17 19:58:34 +0200

    Wrap children and Analytics component in Suspense for improved loading behavior

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from 'next/font/google'
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import { Suspense } from "react";
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -17,11 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+     
       <body
        className={` ${inter.className} antialiased`}
-      >
+      > <Suspense>
         {children}
         <Analytics/>
+        </Suspense>
       </body>
     </html>
   );
